a89d50f02af898e358796bcb988327a9b659e02e
Richard Marcell <richard.marcell8888@gmail.com>
2024-03-31 19:19:38 +0700

    feat: implement heroes section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,20 +4,28 @@ export default function Home() {
   return (
     <>
       {/* Heroes Section Or Home Start*/}
-      <section className="flex items-center flex-col-reverse">
-        <div className="p-4">
-          <h1 className="font-black text-4xl text-green text-center">
+      <section className="flex items-center flex-col-reverse p-4 gap-4 mt-5">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="font-black md:text-4xl lg:text-5xl text-3xl text-green text-center">
             Menyajikan Kelezatan Kopi Bubuk Terbaik
           </h1>
-          <p></p>
+          <p className="text-center text-sm md:text-lg lg:text-xl lg:px-20">
+            Nikmati kehangatan dan kelezatan dalam setiap hidangan kopi bubuk
+            kami, dipanggang dengan penuh cinta dan disajikan dengan senyuman.
+            Sambut pagi dengan segelas kopi, sambut kehidupan dengan Kopi
+            Sanjaya.
+          </p>
+          <button className="p-4 font-bold bg-green hover:opacity-80 text-white rounded-full">
+            Selengkapnya
+          </button>
         </div>
         <Image
           width={0}
           height={0}
-          src={"/images/heroes-ornament.png"}
+          src={"/images/heroes-ornament.svg"}
           alt="heroes ornament"
           sizes="100%"
-          className="w-[400px] h-[360px]"
+          className="w-[350px] h-[320px]"
         />
       </section>
       {/* Heroes Section Or Home End*/}
